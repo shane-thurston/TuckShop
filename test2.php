2a4b685e1965536a87eea5e45e71354210c2a3a9
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2023-08-18 12:49:52 +1000

    Add files via upload

---
+++ test2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Testing PHP</title>
+	</head>
+	
+	<body>
+		<p>Testing database connection</p>
+		<?php
+			$servername = "fdb28.awardspace.net";
+			$username = "4259529_shaneshop";
+			$password = "Stannies23#";
+			$dbname = "4259529_shaneshop";
+			
+			//Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			
+			//Check connection
+			if ($conn->connect_error) {
+				//Display the error and exit
+				die("Connection failed: ". $conn->connect_error);
+			}
+			else {
+				//Display this text
+				echo "Connection made";
+			}
+			//Close connection
+			$conn->close();
+		?>
+	</body>
+</html>
